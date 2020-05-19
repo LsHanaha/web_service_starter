801a8a7a24f87ef1929de5db237ef0eb0547f895
Kirill Trishin <kirik193@yandex.ru>
2020-05-19 20:10:25 +0300

    Update env.py

--- app/env.py
+++ app/env.py
@@ -1,11 +1,15 @@
+import os
+
 DEBUG = True
 
 HOST_IP = '0.0.0.0'
 HOST_PORT = '8888'
 
 DB_NAME = 'Upos'
-DB_USER = 'username'
-DB_PASSWORD = 'password'
+
+
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 DB_PORT = 5432
 DB_HOST = '0.0.0.0'
